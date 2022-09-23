17df948d8b06bd68aaaa57cf3514b9fbcca30022
fernandafons <nanda.fonseca7@gmail.com>
2022-09-23 23:35:23 +0100

    add global styles, add logo in header

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import { Header } from './components/Header'
+import './global.css'
 export function App() {
 
   return (
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,9 @@
 import styles from './Header.module.css'
-
+import igniteLogo from '../assets/ignite-logo.svg'
 export function Header() {
   return (
     <header className={styles.header}>
-    <strong>Header</strong>
+      <img src={igniteLogo} alt="Logotipo do ignite" />
     </header>
   );
 }
