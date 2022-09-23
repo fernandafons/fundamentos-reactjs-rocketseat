c1106142768c58c181c1f64809343f8c4c01bcd5
fernandafons <nanda.fonseca7@gmail.com>
2022-09-24 00:20:46 +0100

    add sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
 import { useState } from 'react'
 import { Header } from './components/Header'
 import './global.css'
+import styles from './App.module.css'
+import { Sidebar } from './components/Sidebar'
 export function App() {
 
   return (
+    <div>
     <Header />
+    <div className={styles.wrapper}>
+      <Sidebar />
+      <main>
+        <p>teste</p>
+        <p>teste2</p>
+      </main>
+      </div>
+      </div>
   )
 }
 
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,16 @@
+import styles from './Sidebar.module.css';
+
+export function Sidebar() {
+  return (
+    <aside className={styles.sidebar}>
+      <img className={styles.cover} src="https://images.unsplash.com/photo-1661956600684-97d3a4320e45?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50" />
+      <div className={styles.profile}>
+        <strong>Maria Fernanda</strong>
+        <span>Web developer</span>
+</div>
+        <footer>
+          <a href="#">Editar seu perfil</a>
+        </footer>
+    </aside>
+  )
+};
