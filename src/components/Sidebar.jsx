96dd163f73dcee2088c353c78ef5752716b3bfe4
fernandafons <nanda.fonseca7@gmail.com>
2022-09-28 18:20:50 +0100

    add profile picture, adjust sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,3 +1,4 @@
+import { PencilLine } from 'phosphor-react';
 import styles from './Sidebar.module.css';
 
 export function Sidebar() {
@@ -5,11 +6,15 @@ export function Sidebar() {
     <aside className={styles.sidebar}>
       <img className={styles.cover} src="https://images.unsplash.com/photo-1661956600684-97d3a4320e45?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50" />
       <div className={styles.profile}>
+        <img className={styles.avatar} src="https://github.com/fernandafons.png" />
         <strong>Maria Fernanda</strong>
         <span>Web developer</span>
-</div>
+      </div>
         <footer>
-          <a href="#">Editar seu perfil</a>
+          <a href="#">
+            <PencilLine size={20}/>
+            Editar seu perfil
+          </a>
         </footer>
     </aside>
   )
